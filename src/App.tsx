7b53bd00b361d24db63b15300001c41d7303c870
Zac Tolley <zac@thetolleys.com>
2021-04-29 13:18:08 +0100

    Tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { EditThing } from "./EditThing";
 import UserConfirmation from "./UserConfirmation";
 
-const App = () => (
+const App: React.FC = () => (
   <Router
     getUserConfirmation={(message, callback) => {
       UserConfirmation(message, callback);
